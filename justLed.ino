46ec6c71929b135dd486d8930fe00000a61d22e6
Paxxs <10364775+Paxxs@users.noreply.github.com>
2022-12-30 22:03:03 +0800

    添加测温功能

--- justLed.ino
+++ justLed.ino
@@ -3,6 +3,8 @@
 #include <Adafruit_GFX.h>
 #include "logo.h"
 #include <Adafruit_SSD1306.h>
+// k type thermocouple driver
+#include <GyverMAX6675.h>
 
 #define OLED_REST -1
 #define SCREEN_WIDTH 128    // OLED display width, in pixels
@@ -12,15 +14,21 @@
 Adafruit_SSD1306 display0(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 #define INTERVAL_LED 2000
+#define INTERVAL_K_TYPE 1000
 // Arduino pin
 #define LED_PIN 2
 #define CLK_PIN 9  // MAX6675 SCK
 #define CS_PIN 8   // MAX6675 CS
 #define DATA_PIN 7 // MAX6675 SO
 
+GyverMAX6675<CLK_PIN, DATA_PIN, CS_PIN> k_type_sensor;
+
 // led time and state
-unsigned long time_led_1 = 0;
+unsigned long time_led = 0;
+unsigned long time_k_type = 0;
 int led_1_state = LOW;
+float k_type_temp = 0;
+bool k_type_err = true;
 
 void setup()
 {
@@ -36,22 +44,32 @@ void setup()
       delay(5000); // Don't proceed, loop forever
     }
   }
+
+  display0.clearDisplay();
   display0.drawBitmap((display0.width() - logo_width) / 2,
                       (display0.height() - logo_height) / 2,
                       logo_data, logo_width, logo_height, 1);
-  delay(2000); // Pause for 2 seconds
+  display0.display();
+  delay(4000); // Pause for 2 seconds
 
-  testanimate(logo16_data, logo16_width, logo16_height);
+  TestAnimate(logo16_data, logo16_width, logo16_height);
   // delay(500);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  DelayRun(&time_led_1, INTERVAL_LED, []() -> void
+  DelayRun(&time_led, INTERVAL_LED, []() -> void
            {
     led_1_state = led_1_state == LOW ? HIGH : LOW;
-    digitalWrite(LED_PIN, led_1_state); });
+    digitalWrite(LED_PIN, led_1_state); 
+    PrintTime(time_led); });
+
+  // refresh temp
+  DelayRun(&time_k_type, INTERVAL_K_TYPE, &UpdateTemp);
+
+  // display
+  DisplayInfo();
 }
 
 /**
@@ -65,33 +83,65 @@ void DelayRun(unsigned long *time, const int interval, void (*callback)())
 {
   if (millis() > *time + interval)
   {
-    print_time(*time);
+    // PrintTime(*time);
     callback();
     *time = millis();
   }
 }
 
-void print_time(unsigned long time_millis)
+/** 更新温度信息 */
+void UpdateTemp()
+{
+  if (k_type_err = k_type_sensor.readTemp()) // 读取温度
+    k_type_temp = k_type_sensor.getTemp();
+  // Serial.print(sens.getTempInt());   // 或 getTempInt - 整数（无浮点数
+}
+
+// 控制台输出文字
+void PrintTime(unsigned long time_millis)
 {
   Serial.print("Time: ");
   Serial.print(time_millis / 1000);
   Serial.print("s - ");
+
+  Serial.print("Temp: ");
+  if (k_type_err)
+  {
+    Serial.print(k_type_temp);
+    Serial.println(" *C");
+  }
+  else
+    Serial.println("Error");
+}
+
+// 更新显示器
+void DisplayInfo()
+{
   display0.clearDisplay();
 
   display0.setTextSize(1);              // Normal 1:1 pixel scale
   display0.setTextColor(SSD1306_WHITE); // Draw white text
   display0.setCursor(0, 0);             // Start at top-left corner
-  display0.print(F("Time: "));
-  display0.print(time_millis / 1000);
-  display0.print(F("s - "));
-
+  display0.print(F("LED: "));
+  display0.print(time_led / 1000);
+  display0.print(F("s - Tp: "));
   display0.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
-  display0.println("yang");
+  if (k_type_err)                                      // 读取温度
+  {
+    display0.println(k_type_temp);
+  }
+  else
+    display0.println("Error");
 
   display0.setTextSize(2); // Draw 2X-scale text
   display0.setTextColor(SSD1306_WHITE);
   display0.print("Baby");
   display0.println(F(" Yang"));
+
+  display0.setTextSize(1); // Draw 1X-scale text
+  display0.setTextColor(SSD1306_WHITE);
+  display0.print("Tp Refresh:");
+  display0.println(time_k_type / 1000);
 
   display0.display();
 }
@@ -101,7 +151,7 @@ void print_time(unsigned long time_millis)
 #define YPOS 1
 #define DELTAY 2
 
-void testanimate(const uint8_t *bitmap, uint8_t w, uint8_t h)
+void TestAnimate(const uint8_t *bitmap, uint8_t w, uint8_t h)
 {
   int8_t f, icons[NUMFLAKES][3];
 
